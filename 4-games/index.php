b27d3a2c74e2a85116c7eb76e018cc445927546f
Pavle Marinkovic <payche255@gmail.com>
2020-10-19 01:22:49 +0200

    First commit

---
+++ 4-games/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Games</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<!--  header  -->
+    <header>
+        <div class="topBar container">
+            <p>Follow us: 
+            <a href=""><i class="fab fa-pinterest-square"></i></a>
+            <a href=""><i class="fab fa-facebook"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+            <a href=""><i class="fab fa-dribbble"></i></a></p>
+        </div>
+        <nav class="container">
+            <a href="" class="logo">Gaming <span>Theme</span></a>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Reviews</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="uvp container">
+            <h1>Game on</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br> Enim rerum illo, delectus rem sint hic. doren bla bla</p>
+            <button>Read more</button>
+        </div>
+    </header>
+    
+<!--  reviews  -->
+    <section class="reviews">
+        <div class="container">
+            <article>
+                <h4>06.10.2020. / <span>Games</span></h4>
+                <h2>The best online game</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit qui aperiam dolor accusantium eum ad quidem, facilis assumenda praesentium perspiciatis recusandae ut debitis itaque quaerat reprehenderit! Ipsum voluptatum quas commodi.</p>
+                <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+            </article>
+              <article>
+                <h4>06.10.2020. / <span>Games</span></h4>
+                <h2>Top games in october</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit qui aperiam dolor accusantium eum ad quidem, facilis assumenda praesentium perspiciatis recusandae ut debitis itaque quaerat reprehenderit! Ipsum voluptatum quas commodi.</p>
+                <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+            </article>
+              <article>
+                <h4>06.10.2020. / <span>Games</span></h4>
+                <h2>Get this game, as soon</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit qui aperiam dolor accusantium eum ad quidem, facilis assumenda praesentium perspiciatis recusandae ut debitis itaque quaerat reprehenderit! Ipsum voluptatum quas commodi.</p>
+                <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+            </article>
+        </div>
+    </section>
+
+<!--Novosti-->
+
+    <section class="news">
+        <div class="container">
+            <article>
+                <h2>Latest news</h2>
+                <div class="posts">
+                    <div class="featured-images">
+                        <img src="img/blog1.jpg" alt="">
+                    </div>
+                    <div class="text">
+                        <h4>06.10.2020 / <span>Games</span></h4>
+                        <h3>Blog poste one</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sequi quasi in officiis. Deserunt consectetur laboriosam voluptatem ab esse eum!</p>
+                        <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+                <div class="posts">
+                    <div class="featured-images">
+                        <img src="img/blog2.jpg" alt="">
+                    </div>
+                    <div class="text">
+                        <h4>06.10.2020 / <span>Games</span></h4>
+                        <h3>Blog poste two</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sequi quasi in officiis. Deserunt consectetur laboriosam voluptatem ab esse eum!</p>
+                        <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+                <div class="posts">
+                    <div class="featured-images">
+                        <img src="img/blog3.jpg" alt="">
+                    </div>
+                    <div class="text">
+                        <h4>06.10.2020 / <span>Games</span></h4>
+                        <h3>Blog poste three</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sequi quasi in officiis. Deserunt consectetur laboriosam voluptatem ab esse eum!</p>
+                        <a href="" class="links">Read more&nbsp;<i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </article>
+            <aside>
+                <div class="widget">
+                    <h3>Categories</h3>
+                     <ul>
+                        <li><a href="">Nitendo</a></li>
+                        <li><a href="">Sony</a></li>
+                        <li><a href="">Xbox</a></li>
+                        <li><a href="">Diablo</a></li>
+                        <li><a href="">Warcraft</a></li>
+                        <li><a href="">Age of emperies</a></li>
+                        <li><a href="">Super mario</a></li>
+                    </ul>
+                </div>
+                <div class="widget">
+                    <div>
+                        <img src="img/blog1.jpg" alt="">
+                        <h4>Super offer</h4>
+                        <h2>90% off</h2>
+                        <button>Get it now</button>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </section>
+    
+<!--   subscribe -->
+    
+        <section class="subscribe">
+            <h2>Subscribe to our newsletter</h2>
+            <input type="email" placeholder="Enter email">
+            <input type="submit" value="Send">
+        </section>
+
+<!--footer-->
+        <footer>
+            <a href="" class="logo">Gaming <span>Theme</span></a>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Reviews</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+             <p>Follow us: 
+            <a href=""><i class="fab fa-pinterest-square"></i></a>
+            <a href=""><i class="fab fa-facebook"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+            <a href=""><i class="fab fa-dribbble"></i></a></p>
+        </footer>
+    
+    
+</body>
+
+</html>
